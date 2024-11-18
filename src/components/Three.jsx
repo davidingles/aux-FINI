2a4042205522eb1b5b8878344b4573ad8d98c69e
davidingles <davidingleslopez@gmail.com>
2024-11-18 16:03:48 +0100

    fini2

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -15,7 +15,7 @@ const Models = [
   // { title: 'antorcha2', url: './antorcha2.glb', miEscala: .5, miPosicion: -0.3 },
   // { title: 'antorcha1', url: './antorcha1.glb', miEscala: .5, miPosicion: -0.3 },
   // { title: '1g0004', url: './1g0004.glb', miEscala: .6, miPosicion: .0 },
-  { title: 'PRIVE', url: './PRIVE.glb', miEscala: 1.0, miPosicion: -.0 },
+  { title: 'FINI', url: './FINI.glb', miEscala: 1.0, miPosicion: -.0 },
   // { title: 'CESTA1', url: './CESTA1.glb', miEscala: .9, miPosicion: -.3 },
   // { title: 'FRUTOS', url: './FRUTOS.glb', miEscala: 1.3, miPosicion: .2 },
 ]
